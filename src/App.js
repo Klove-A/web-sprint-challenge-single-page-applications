f0b7da85858710b5aae4d553eefc3f523cbd55c4
Klove Adams <kalebsxott@gmail.com>
2021-07-23 11:51:17 -0400

    added initialFormValues and imports to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Route, Link, Switch, Redirect } from "react-router-dom"
+
+const initialFormValues = {
+  name: "",
+  size: "",
+  topping1: false,
+  topping2: false,
+  topping3: false,
+  topping4: false,
+  special: "",
+}
 
 const App = () => {
   return (
